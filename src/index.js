ea90a5e794db24aa1e4f98718dde69124b5c8418
Jasper Alblas <just_jasper@hotmail.com>
2024-08-14 21:34:46 +0200

    Simple hovering mechanics added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./style.css";
+
+const dropdownContent = document.querySelector(".dropdown-content");
+const dropdownButton = document.querySelector(".dropdown-button");
+
+dropdownButton.addEventListener("mouseenter", () => {
+  console.log("IS THIS CALLED?");
+  dropdownContent.classList.toggle("hidden");
+});
+
+dropdownButton.addEventListener("mouseleave", () => {
+  console.log("IS THIS CALLED?");
+  dropdownContent.classList.toggle("hidden");
+});
