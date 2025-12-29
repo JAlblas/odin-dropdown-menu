c6d95e9237aead6c6a44830095f7bd561095b577
Jasper Alblas <just_jasper@hotmail.com>
2025-12-29 15:44:39 +0100

    Some minor changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import "./style.css";
 
 const dropdownMenus = document.querySelectorAll(".dropdown-menu");
-const dropdownContents = document.querySelectorAll(".dropdown-content");
 const dropdownButtons = document.querySelectorAll(".dropdown-button");
 
 dropdownButtons.forEach((button) => {
   button.addEventListener("mouseenter", () => {
     const dropdownContent =
       button.parentElement.querySelector(".dropdown-content");
-    dropdownContent.classList.toggle("hidden");
+    dropdownContent.classList.remove("hidden");
   });
 });
 
 dropdownMenus.forEach((menu) => {
-  console.log(menu);
   menu.addEventListener("mouseleave", () => {
     const content = menu.querySelector(".dropdown-content");
-    content.classList.toggle("hidden");
+    content.classList.add("hidden");
   });
 });
